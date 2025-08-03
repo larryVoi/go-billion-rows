37a8b91d7f0f5760a6d929faac9aa2d5fe2e7201
VictoriqueMoe <loli@victorique.moe>
2025-08-03 21:38:47 +0100

    init commit

--- mmap_unix.go
+++ mmap_unix.go
@@ -7,7 +7,6 @@ import (
 	"syscall"
 )
 
-// mmapFile maps the given file into memory using Unix-specific syscalls.
 func mmapFile(file *os.File) ([]byte, func(), error) {
 	fi, err := file.Stat()
 	if err != nil {
@@ -15,13 +14,11 @@ func mmapFile(file *os.File) ([]byte, func(), error) {
 	}
 	fileSize := fi.Size()
 
-	// Memory-map the file.
 	data, err := syscall.Mmap(int(file.Fd()), 0, int(fileSize), syscall.PROT_READ, syscall.MAP_SHARED)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	// Return a cleanup function that the caller should defer.
 	cleanup := func() {
 		syscall.Munmap(data)
 	}
